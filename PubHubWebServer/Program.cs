b1958bdc8a3a328e0bd5bba695ecb9498ce672f5
Jens Anker Bruun de Neergaard <jens5561@elevcampus.dk>
2024-04-10 14:28:34 +0200

    Cant press buttons

--- PubHubWebServer/Program.cs
+++ PubHubWebServer/Program.cs
@@ -55,7 +55,7 @@ namespace PubHubWebServer
             builder.Services.AddSwaggerGen();
 
             builder.Services.AddHttpClient();//Added HttpClient as a service
-            
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -421,6 +421,15 @@ namespace PubHubWebServer
 
                 return Results.Unauthorized();
             }).WithTags("ebooks").RequireAuthorization();
+
+            app.MapGet("bookList/GetBooksFilter/",
+                async (ClaimsPrincipal user, IPubHubServices pubHubServices,
+                 int skip, int take, string title = "", string author = "", string genre = "") =>
+                {
+                    var response = await pubHubServices.GetBooksByFilter(title, author, genre, skip, take);
+                    return Results.Json(response);
+
+                }).WithTags("ebooks");
 
             #endregion
 
