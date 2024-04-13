77e7bd72da24d6ac99857cf05cd01d0bc2c4c429
Lukas Nørskov Pedersen <luka0591@elevcampus.dk>
2024-04-13 11:44:23 +0200

    Fhinished Singlebook.

--- PubHubWebServer/Migrations/20240413081117_FixedSpellingMistake.Designer.cs
+++ PubHubWebServer/Migrations/20240413081117_FixedSpellingMistake.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PubHubWebServer.Data;
 
@@ -11,9 +12,11 @@ using PubHubWebServer.Data;
 namespace PubHubWebServer.Migrations
 {
     [DbContext(typeof(PubHubDBContext))]
-    partial class PubHubDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240413081117_FixedSpellingMistake")]
+    partial class FixedSpellingMistake
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
